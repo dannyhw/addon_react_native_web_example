c395c5595d6ff14da77cedf8146738136e134d93
Danny <dannyhyunsoowilliams@gmail.com>
2022-05-10 14:41:41 +0100

    upgrade react native files

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import {NativeBaseProvider} from 'native-base';
 import React from 'react';
 import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
-import {TextField} from 'react-native-ui-lib';
+
 import {Draggable} from './components/libraries/Gesture/Draggable/Draggable';
 import {AppBar} from './components/libraries/NativeBase/AppBar';
 import {Card} from './components/libraries/Paper/Card';
@@ -13,29 +13,24 @@ const App = () => {
   return (
     <NativeBaseProvider>
       <PaperProvider theme={DefaultTheme}>
-        <SafeAreaView style={styles.container}>
-          <AppBar />
-          <ScrollView>
-            <View style={styles.center}>
-              <Box />
-              <Draggable style={styles.draggable}>
-                <Text>Drag me</Text>
-              </Draggable>
-              <View style={styles.cardContainer}>
-                <Card
-                  content="test content"
-                  onCancel={() => null}
-                  onConfirm={() => null}
-                  title="test"
-                />
-              </View>
-              <Icon name="account" />
-              <View style={styles.textContainer}>
-                <TextField />
-              </View>
+        <AppBar />
+        <ScrollView>
+          <View style={styles.center}>
+            <Box />
+            <Draggable style={styles.draggable}>
+              <Text>Drag me</Text>
+            </Draggable>
+            <View style={styles.cardContainer}>
+              <Card
+                content="test content"
+                onCancel={() => null}
+                onConfirm={() => null}
+                title="test"
+              />
             </View>
-          </ScrollView>
-        </SafeAreaView>
+            <Icon name="account" />
+          </View>
+        </ScrollView>
       </PaperProvider>
     </NativeBaseProvider>
   );
@@ -51,6 +46,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 8,
     borderRadius: 8,
+    zIndex: 10,
   },
   cardContainer: {width: '90%'},
   center: {alignItems: 'center'},
